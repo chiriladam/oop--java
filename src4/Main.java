580fc822fcb3392f4655d08145ddab97e0b29c58
chiriladam <chiriltiktok@gmail.com>
2025-12-03 18:17:39 +0200

    Create Main.java

---
+++ src4/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Simple manual creation for Day 1 testing
+        AlienSpecies a = new AlienSpecies(
+                1,
+                "Zorblax",
+                "medium",
+                new String[]{"tentacles", "acid saliva"},
+                45
+        );
+
+        System.out.println(a);
+    }
+}
