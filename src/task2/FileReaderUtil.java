698622f48557cc30349bf7a4afe720a74b91c3be
chiriladam <chiriltiktok@gmail.com>
2025-12-03 15:53:40 +0200

    Update FileReaderUtil.java

--- src/task2/FileReaderUtil.java
+++ src/task2/FileReaderUtil.java
@@ -3,11 +3,17 @@ package task2;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+/*
+ * Utility class to read entire text files into a String.
+ */
 public class FileReaderUtil {
+
+    // Reads file content as a complete string
     public static String readFileIntoString(String path) {
         try {
             return Files.readString(Paths.get(path));
         } catch (Exception e) {
+            System.out.println("Error reading file: " + path);
             return "";
         }
     }
