a3618e22706b99ddd20caf129e73c716612e9282
chiriladam <chiriltiktok@gmail.com>
2025-12-04 10:01:57 +0200

    Update Americano.java

--- src5/lab5/coffee/Americano.java
+++ src5/lab5/coffee/Americano.java
@@ -7,10 +7,23 @@ package lab5.coffee;
 public class Americano extends Coffee {
 
     protected int mlOfWater;
-    protected final String coffeeName = "Americano";
+    protected final String coffee = "Americano";
 
     public Americano(Intensity intensity, int mlOfWater) {
         super(intensity);
         this.mlOfWater = mlOfWater;
+    }
+
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Water: " + mlOfWater + " ml");
+    }
+
+    public Americano makeAmericano() {
+        System.out.println("Making Americano:");
+        System.out.println(" - Brewing espresso");
+        System.out.println(" - Adding " + mlOfWater + " ml hot water");
+        return this;
     }
 }
